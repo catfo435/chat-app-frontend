945b24272ce9abdfd647fd529f3516a0c10f07fe
catfo <pranavlsgreat210120@gmail.com>
2024-02-18 19:55:59 +0530

    chat functionality implementation star

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,38 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 
+const socket = new WebSocket("ws://localhost:5000")
+
 export default function Page() {
 
-  const [users,setUsers] = useState([])
-  
+  const [messages,setMessages] = useState<Array<any>>([])
+  const [messageVal,setMessageVal] = useState("")
+
   useEffect(() => {
-    fetch("http://localhost:5000/users").then(
-      res => res.json().then(
-        data => {
-          console.log(data);
-          
-          setUsers(data.users)
-        }
-      )
-    )
+    socket.onmessage = ({data}) => {
+    console.log(data)
+    setMessages(messages => {
+      messages=[...messages,data]
+      return messages
+    })
+    }
   },[])
+
+  function handleSendMessage(){
+    socket.send(messageVal)
+    setMessageVal("")
+  }
 
   return (
     <div className='content flex justify-center items-center w-screen h-screen'>
       <div className='flex flex-col justify-center items-center w-2/3 h-[600px] border-2 border-blue-500 rounded-lg'>
-        <div className='chatWindow flex w-full h-2/3 bg-slate-400'>
-
+        <div className='chatWindow flex flex-col w-full h-2/3 bg-slate-400'>
+          {messages.map((message,index) => (<div key={index}>{message}</div>))}
         </div>
         <div className='messageBar flex flex-col items-center w-full h-1/3'>
-          <input className='messageInput w-full bg-gray-200 h-1/2 p-2 mb-4' placeholder='Enter a message'>
+          <input className='messageInput w-full bg-gray-200 h-1/2 p-2 mb-4' value={messageVal} onChange={(e) => {setMessageVal(e.target.value)}} placeholder='Enter a message'>
           </input>
-          <button className='sendButton bg-gray-400 w-20 h-10'>Send</button>
+          <button className='sendButton bg-gray-400 w-20 h-10' onClick={handleSendMessage}>Send</button>
         </div>
       </div>
     </div>
