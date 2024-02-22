36a0a7525645013862f98edd11920ea019872893
catfo <pranavlsgreat210120@gmail.com>
2024-02-22 13:49:46 +0530

    fix: server api hidden by a env path

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-
-const socket = new WebSocket("ws://localhost:5000")
+ 
+const socket = new WebSocket(process.env.NEXT_PUBLIC_APIURL!)
 
 export default function Page() {
 
